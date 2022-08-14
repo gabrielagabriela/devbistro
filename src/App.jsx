560f15aa939c9657c0400bbc3f60a8d0f018d379
gabrielagabriela <gabrielaa.slv@gmail.com>
2022-08-14 16:52:04 -0300

    03_exercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Footer } from './assets/components/Footer'
 import { Header } from './assets/components/Header'
 
 function App() {
  
 
   return (
+  <div className='app'>
    <Header />
+   <Footer />
+  </div>
   )
 }
 
---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,14 @@
+export function Footer () {
+  return (
+    <footer>
+      <div className="endereco">
+        <p>Endereço : <br /> Rua do Centro, Florianópolis 88</p>
+        <p>Telefone: <br /> (48) 999-9999</p>
+        <p>Horário de Funcionamento: <br/> ter - dom : 18:30 às 00:30</p>
+      </div>
+      <div className="copy">
+        <p>Copyright 2022</p>
+      </div>
+    </footer>
+  )
+}
